bd3b43bfa16ec611f83fc82b913d70d0a32a995c
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-03-15 18:33:03 +0530

    Add release helper

---
+++ release_helper/api/index.ts
@@ -0,0 +1,19 @@
+import { ServerRequest } from "https://deno.land/std@0.90.0/http/server.ts"
+import getReleaseURL from "https://deno.land/x/get_release_url@1.0.0/mod.ts"
+
+export default async (req: ServerRequest) => {
+    const [url] = await getReleaseURL({
+        provider: "github",
+        user: "KhushrajRathod",
+        repo: "repl.deploy",
+        part: "Linux",
+    })
+
+    req.respond({
+        status: 302,
+        headers: new Headers({
+            "Location": url,
+            "Cache-Control": "s-maxage=300",
+        }),
+    })
+}
