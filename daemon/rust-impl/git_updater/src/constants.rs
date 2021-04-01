c012d4dfc4f1a513244bd1972bfa79f439ba4692
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-04-01 16:03:16 +0530

    Implement git updater

---
+++ daemon/rust-impl/git_updater/src/constants.rs
@@ -0,0 +1,2 @@
+pub const GIT_FETCH_FAILED_ERROR: &str = "'git fetch --all' failed";
+pub const GIT_RESET_FAILED_ERROR: &str = "'git reset --hard origin/main' failed";
--- daemon/rust-impl/git_updater/src/lib.rs
+++ daemon/rust-impl/git_updater/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod constants;
+
+use {
+    constants::*,
+    logger,
+    std::{io, process::Command},
+};
+
+pub fn update_git_from_remote() -> Result<(), io::Error> {
+    let git_fetch = Command::new("git").args(&["fetch", "--all"]).output();
+
+    if let Err(e) = git_fetch {
+        logger::error(GIT_FETCH_FAILED_ERROR);
+        return Err(e);
     }
+
+    let git_reset = Command::new("git")
+        .args(&["reset", "--hard", "origin/main"])
+        .output();
+
+    if let Err(e) = git_reset {
+        logger::error(GIT_RESET_FAILED_ERROR);
+        return Err(e);
+    }
+
+    Ok(())
 }
