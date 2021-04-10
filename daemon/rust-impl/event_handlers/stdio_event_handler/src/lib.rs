ef24a132d5386391491b62df3abe2088e60d7267
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-04-10 13:04:41 +0530

    Implement stdio_event_handler

--- daemon/rust-impl/event_handlers/stdio_event_handler/src/lib.rs
+++ daemon/rust-impl/event_handlers/stdio_event_handler/src/lib.rs
@@ -1,7 +1,132 @@
+use {
+	constants::{
+		PROBLEMS_SERIALIZING_JSON_ERROR, PROBLEMS_WRITING_TO_STDIN_OF_SUBPROCESS_ERROR,
+		STAT_REQUEST_RECEIVED, STAT_SIGNATURE_VALIDATION_FAILED, STAT_SIGNATURE_VALIDATION_SUCCESS,
+		STDIN_REGEX, STDIN_RESPONDED_SUCCESSFULLY, STDIN_SUCCESS,
+	},
+	logger,
+	regex::Regex,
+	rsa::RSAPublicKey,
+	serde_json, signature_verifier,
+	std::{
+		io::{BufRead, BufReader, Write},
+		process::Child,
+	},
+	types::{Config, ValidationResult},
+};
+
+pub fn listen<'a>(
+	config: &Config,
+	public_key: &RSAPublicKey,
+	handler: impl Fn() -> Result<&'a mut Child, &'static str>,
+	cmd: &mut Child,
+) {
+	let mut child_ref = cmd;
+	let stdin_regex = Regex::new(STDIN_REGEX).unwrap();
+	loop {
+		scan_process_stdout_until_successful_request(child_ref, &stdin_regex, config, public_key);
+
+		match handler() {
+			Ok(new_cmd) => {
+				child_ref = new_cmd;
+			}
+			Err(e) => logger::error(e),
+		}
+	}
+}
+
+fn scan_process_stdout_until_successful_request(
+	cmd: &mut Child,
+	stdin_regex: &Regex,
+	config: &Config,
+	public_key: &RSAPublicKey,
+) {
+	let reader = BufReader::new(cmd.stdout.as_mut().unwrap());
+	let writer = cmd.stdin.as_mut().unwrap();
+
+	for line in reader.lines() {
+		match line {
+			Ok(line) => {
+				if line == STDIN_SUCCESS {
+					logger::success(STDIN_RESPONDED_SUCCESSFULLY);
+					break;
+				}
+
+				match stdin_regex.captures(&line) {
+					Some(matches) => {
+						logger::info(STAT_REQUEST_RECEIVED);
+
+						let payload = &matches[1];
+						let input_signature = &matches[2];
+
+						let response =
+							match validation_result_to_string(validate_and_return_response(
+								payload,
+								input_signature,
+								config,
+								public_key,
+							)) {
+								Some(r) => r,
+								None => continue,
+							};
+
+						write_response(response.as_bytes(), writer);
+					}
+					None => println!("{}", &line),
+				}
+			}
+			Err(_) => logger::error("Failed to read subprocess stdout"),
+		}
+	}
+}
+
+fn validate_and_return_response(
+	payload: &str,
+	input_signature: &str,
+	config: &Config,
+	public_key: &RSAPublicKey,
+) -> ValidationResult<'static> {
+	match signature_verifier::validate_payload_and_signature(
+		payload,
+		input_signature,
+		config,
+		public_key,
+	) {
+		Ok(()) => {
+			logger::success(STAT_SIGNATURE_VALIDATION_SUCCESS);
+
+			return ValidationResult {
+				body: "OK",
+				status: 200,
+			};
+		}
+		Err(e) => {
+			logger::warn(STAT_SIGNATURE_VALIDATION_FAILED);
+			return e;
+		}
+	};
+}
+
+fn validation_result_to_string(r: ValidationResult) -> Option<String> {
+	match serde_json::to_string(&r) {
+		Ok(json) => Some(json),
+		Err(_) => {
+			logger::error(PROBLEMS_SERIALIZING_JSON_ERROR);
+			None
+		}
+	}
+}
+
+fn write_response(response: &[u8], writer: &mut std::process::ChildStdin) {
+	if let Err(_) = writer.write(response) {
+		logger::error(PROBLEMS_WRITING_TO_STDIN_OF_SUBPROCESS_ERROR)
+	};
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	#[test]
+	fn it_works() {
+		assert_eq!(2 + 2, 4);
+	}
 }
--- daemon/rust-impl/static/constants/src/lib.rs
+++ daemon/rust-impl/static/constants/src/lib.rs
@@ -1,9 +1,12 @@
 // Strings
 pub const REPLIT_DEPLOY_JSON_PATH: &str = "./replit-deploy.json";
 pub const SIGNATURE_HEADER_NAME: &str = "Signature";
+pub const STDIN_REGEX: &str = r"repl\.deploy({.*})(.*)";
+pub const STDIN_SUCCESS: &str = "repl.deploy-success";
+pub const STDIN_RESPONDED_SUCCESSFULLY: &str = "Responded successfully";
 
 // Errors
-pub const PUBLIC_KEY_PARSE_ERROR: &str = 
+pub const PUBLIC_KEY_PARSE_ERROR: &str =
     "Failed to parse public key. This shouldn't have happened, please open a new issue at https://github.com/KhushrajRathod/repl.deploy/issues/new";
 pub const MISSING_CONFIG_FILE_ERROR: &str = "Config file doesn't exist";
 pub const INVALID_JSON_ERROR: &str = "Invalid config JSON";
@@ -13,12 +16,17 @@ pub const INVALID_SIGNATURE_ERROR: &str = "Invalid Signature";
 pub const BAD_PAYLOAD_ERROR: &str = "Bad payload";
 pub const PAYLOAD_TOO_OLD_ERROR: &str = "Payload too old";
 pub const BAD_ENDPOINT_ERROR: &str = "Signed request not intended for current endpoint";
+pub const FAILED_TO_START_CHILD_PROCESS_ERROR: &str = "Failed to start child process";
+pub const PROBLEMS_SERIALIZING_JSON_ERROR: &str = "Problems serializing JSON";
+pub const PROBLEMS_WRITING_TO_STDIN_OF_SUBPROCESS_ERROR: &str =
+    "Problems writing to stdin of subprocess";
 
 // Warnings
 pub const SIGNATURE_VALIDATION_FAILED_WARN: &str =
     "Signature validation failed for an event, so listeners will not be called";
 
 // Status
+pub const STAT_PROGRAM_STARTED: &str = "Program has been started.";
 pub const STAT_REQUEST_RECEIVED: &str = "Received restart request from application, processing...";
 pub const STAT_SIGNATURE_VALIDATION_FAILED: &str =
     "Signature validation failed, restart will not be triggered";
