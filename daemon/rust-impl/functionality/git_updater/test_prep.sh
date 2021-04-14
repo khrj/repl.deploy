b977068e77dbd3a388d32d849d6bffbb587c1dc7
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-04-14 19:00:37 +0530

    Fix git config commands to config for correct repo

--- daemon/rust-impl/functionality/git_updater/test_prep.sh
+++ daemon/rust-impl/functionality/git_updater/test_prep.sh
@@ -4,16 +4,17 @@ set -e
 # Clean old test results
 rm -rf test_repo1 test_repo2
 
-git config user.email "test-runner@example.com"
-git config user.name "Test Runner"
-
 mkdir test_repo1
 cd test_repo1
 git init
+
 cd ..
 git clone ./test_repo1 test_repo2
 cd test_repo1
 printf "hi" > temp
+
+git config user.email "test-runner@example.com"
+git config user.name "Test Runner"
 git add temp
 git commit -m test_commit
 git branch -m main
